08ee50b10dbd5e7da68471cc4ca707170825799a
hommym <herbertharthur80@gmail.com>
2026-06-30 18:31:47 +0000

    Add frustum culling, screen-coord helper, and wireframe culled-vertex skip

--- src/renderer.c
+++ src/renderer.c
@@ -18,26 +18,48 @@ CameraPos camera_position={
 
 
 
-
 static void* create_frame_buffer(uint32_t win_w,uint32_t win_h){
 return calloc((win_h*win_w),4);
 }
 
+static void calc_screen_cordinate(double win_size,double start_cord,double* end_cord_p){
+    // calculates x or y cordinate values for the screen in world space(ie 3D)
+    double diff=start_cord-(*end_cord_p);
+    if(diff>win_size){
+    *end_cord_p-=fabs(diff-win_size);
+    }
+    else if(diff<win_size){
+    *end_cord_p+=fabs(diff-win_size);
+    }
+    else *end_cord_p=diff+start_cord;
 
+}
 
 
 void * render(Object* objects,uint64_t len,uint32_t win_w,uint32_t win_h,bool wirefame_mode){
 uint32_t (*frame_buffer)[win_w]= (uint32_t (*)[win_w]) create_frame_buffer(win_w,win_h);
+calc_screen_cordinate(win_w,camera_position.x,&(camera_position.x_end));
+calc_screen_cordinate(win_h,camera_position.y,&(camera_position.y_end));
+
+
 
 // project 3D points to 2D
 for(uint64_t i=0;i<len;i++){
     for(uint64_t a=0;a<objects[i].len_of_vertices;a++){
-       
+       Vectex point=objects[i].vertices[a];
+       bool is_x_in_view=point.x>=camera_position.x && point.x<=camera_position.x_end;
+       bool is_y_in_view=point.y>=camera_position.y && point.y<=camera_position.y_end;
+       bool is_z_in_view=point.z>=camera_position.z && point.z<=camera_position.z_end;
+
+       if(!(is_x_in_view && is_y_in_view && is_z_in_view))continue; 
+
+        
+
         objects[i].vertices[a].px=perspective_projection(objects[i].vertices[a].x,objects[i].vertices[a].z,focal_len,camera_position.x,camera_position.x_end);
         objects[i].vertices[a].py=perspective_projection(objects[i].vertices[a].y,objects[i].vertices[a].z,focal_len,camera_position.y,camera_position.y_end);
         uint64_t px=objects[i].vertices[a].px;
         uint64_t py=objects[i].vertices[a].py;
-        if(px<win_w && py<win_h) frame_buffer[py][px]=objects[i].colour;
+        // frame_buffer[py][px]=objects[i].colour;
     }
     
 
@@ -51,6 +73,7 @@ Object obj=objects[x];
 for(size_t a=0;a<obj.len_of_connectors;a+=2){
     Vectex v1=obj.vertices[obj.connectors_sequence[a]];
     Vectex v2=obj.vertices[obj.connectors_sequence[a+1]];
+    if((v1.px==0 && v1.py==0) || (v2.px==0 && v2.py==0))continue;
     int64_t ch_x=v1.px-v2.px;
     int64_t ch_y=v1.py-v2.py;
   
