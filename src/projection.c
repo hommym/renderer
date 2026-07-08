62bf22228e6e216be203584409a2aa59b97832ce
hommym <herbertharthur80@gmail.com>
2026-07-08 08:41:35 +0000

    Clamp projected pixel to screen bounds in projection

--- src/projection.c
+++ src/projection.c
@@ -7,11 +7,21 @@
 uint64_t perspective_projection(double xy,double z,int32_t focal,double start_p,double end_p){
 double centre     = start_p + (end_p - start_p)/2.0;   // world center
 double pix_centre = (end_p - start_p)/2.0;             // screen center pixel
-double offset     = xy - centre;
+double offset;
+
+offset=xy-centre;
+
 if(z != 0){
     offset = (offset * focal) / z;
 }
-return (uint64_t)(pix_centre + offset);
+
+double pix_point=pix_centre+offset;
+
+// cliping to screen in pixel space
+if (pix_point<0)pix_point=0.0;
+else if(pix_point>(end_p-start_p)) pix_point=end_p-start_p;
+
+return (uint64_t) pix_point;
 }
 
 
